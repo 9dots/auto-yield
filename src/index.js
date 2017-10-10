16c608c6d01288fc9714a74cf30e08e4d1830d5d
danleavitt0 <danleavitt0@gmail.com>
2017-10-09 17:15:52 -0700

    fixes support for nested function calls

--- src/index.js
+++ src/index.js
@@ -12,36 +12,32 @@ var t = babel.types
  */
 
 module.exports = autoYield
-function addLineNumber (path) {
-  return t.callExpression(
-    t.identifier(path.node.callee.name),
-    [t.numericLiteral(path.node.callee.loc.start.line), ...path.node.arguments]
-  )
+function addLineNumber(path) {
+  return t.callExpression(t.identifier(path.node.callee.name), [
+    t.numericLiteral(path.node.callee.loc.start.line),
+    ...path.node.arguments
+  ])
 }
 
 function addCallFnWrapper(path, userFnName) {
-  return t.callExpression(
-    t.identifier(userFnName),
-    [
-      t.numericLiteral(path.node.callee.loc.start.line),
-      t.identifier(path.node.callee.name),
-      ...path.node.arguments
-    ]
-  )
+  return t.callExpression(t.identifier(userFnName), [
+    t.numericLiteral(path.node.callee.loc.start.line),
+    t.identifier(path.node.callee.name),
+    ...path.node.arguments
+  ])
 }
 
 /**
  * auto-yield
  */
 
-function autoYield (code, generatorNames, secondOrderGens, userFnName) {
+function autoYield(code, generatorNames, secondOrderGens, userFnName) {
   generatorNames = generatorNames || []
   secondOrderGens = secondOrderGens || []
 
-
   var withoutArrows = babel.transform(code, {
     retainLines: true,
-    plugins: [ arrowFunctions ]
+    plugins: [arrowFunctions]
   })
 
   const visitor = {
@@ -57,37 +53,59 @@ function autoYield (code, generatorNames, secondOrderGens, userFnName) {
 
   return result.code
 
-  function CallExpression (path) {
+  function CallExpression(path) {
     var parent = path.parentPath
     if (parent.node.type !== 'YieldExpression') {
       if (path.node.callee.type !== 'MemberExpression') {
-        const inScope = path.scope.bindings[path.node.callee.name]
-          && path.scope.bindings[path.node.callee.name].type
-          && path.scope.bindings[path.node.callee.name].type !== 'param'
+        const inScope =
+          path.scope.bindings[path.node.callee.name] &&
+          // path.scope.bindings[path.node.callee.name].type &&
+          path.scope.bindings[path.node.callee.name].type !== 'param'
         const inFile = path.hub.file.scope.bindings[path.node.callee.name]
-        // console.log(path.scope.bindings[path.node.callee.name].type, path.node.callee.name)
         const deleg = inScope || inFile ? true : false
-        path.replaceWith(babel.types.yieldExpression(deleg ? addCallFnWrapper(path, userFnName) : addLineNumber(path), deleg))
+        path.replaceWith(
+          babel.types.yieldExpression(
+            deleg ? addCallFnWrapper(path, userFnName) : addLineNumber(path),
+            deleg
+          )
+        )
       }
     }
   }
 
-  function FunctionDeclaration (path) {
+  function FunctionDeclaration(path) {
     if (!path.node.generator) {
-      path.replaceWith(babel.types[camelCase(path.node.type)](path.node.id, path.node.params, path.node.body, true))
+      path.replaceWith(
+        babel.types[camelCase(path.node.type)](
+          path.node.id,
+          path.node.params,
+          path.node.body,
+          true
+        )
+      )
     }
   }
 
-  function VariableDeclarator (path) {
-    if (path.node.init.type === 'FunctionExpression' && !path.node.init.generator) {
-      path.replaceWith(babel.types.variableDeclarator(
-        path.node.id,
-        babel.types[camelCase(path.node.init.type)](path.node.init.id, path.node.init.params, path.node.init.body, true)
-      ))
+  function VariableDeclarator(path) {
+    if (
+      path.node.init.type === 'FunctionExpression' &&
+      !path.node.init.generator
+    ) {
+      path.replaceWith(
+        babel.types.variableDeclarator(
+          path.node.id,
+          babel.types[camelCase(path.node.init.type)](
+            path.node.init.id,
+            path.node.init.params,
+            path.node.init.body,
+            true
+          )
+        )
+      )
     }
   }
 
-  function isGenerator (callee, scope) {
+  function isGenerator(callee, scope) {
     if (callee.object) {
       return isMethodGenerator(callee.object.name, callee.property.name, scope)
     } else {
@@ -98,19 +116,22 @@ function autoYield (code, generatorNames, secondOrderGens, userFnName) {
   function isFunctionGenerator(name, scope) {
     scope = findBindingScope(name, scope)
     if (scope) {
-      return scope.bindings[name].path.node.generator || !scope.parent && generatorNames.indexOf(name) >= 0
+      return (
+        scope.bindings[name].path.node.generator ||
+        (!scope.parent && generatorNames.indexOf(name) >= 0)
+      )
     } else {
       return generatorNames.indexOf(name) >= 0
     }
   }
 
-  function isMethodGenerator (objectName, propertyName, scope) {
+  function isMethodGenerator(objectName, propertyName, scope) {
     scope = findBindingScope(objectName, scope)
     //XXX add real support for methods
     return scope && !scope.parent && generatorNames.indexOf(objectName) >= 0
   }
 
-  function findBindingScope (name, scope) {
+  function findBindingScope(name, scope) {
     while (scope && !scope.bindings[name]) {
       scope = scope.parent
     }
