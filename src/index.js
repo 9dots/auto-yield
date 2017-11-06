5b9ac40e5e8914b7a294684681303bd5f64fdc1e
danleavitt0 <danleavitt0@gmail.com>
2017-11-06 14:35:38 -0800

    babel-core -> @babel/standalone

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
  * Imports
  */
 
-var babel = require('babel-core')
-var camelCase = require('camel-case')
-const arrowFunctions = require('babel-plugin-transform-es2015-arrow-functions')
-var t = babel.types
+var babel = require("@babel/standalone");
+var camelCase = require("camel-case");
+const arrowFunctions = require("babel-plugin-transform-es2015-arrow-functions");
+var t = require("babel-types");
 
 /**
  * Expose auto yield
  */
 
-module.exports = autoYield
+module.exports = autoYield;
 function addLineNumber(path) {
   return t.callExpression(t.identifier(path.node.callee.name), [
     t.numericLiteral(path.node.callee.loc.start.line),
     ...path.node.arguments
-  ])
+  ]);
 }
 
 function addCallFnWrapper(path, userFnName) {
@@ -24,51 +24,51 @@ function addCallFnWrapper(path, userFnName) {
     t.numericLiteral(path.node.callee.loc.start.line),
     t.identifier(path.node.callee.name),
     ...path.node.arguments
-  ])
+  ]);
 }
 
 /**
  * auto-yield
  */
 
-function autoYield(code, generatorNames, secondOrderGens, userFnName) {
-  generatorNames = generatorNames || []
-  secondOrderGens = secondOrderGens || []
+function autoYield(code, generatorNames, secondOrderGens, userFnName = "") {
+  generatorNames = generatorNames || [];
+  secondOrderGens = secondOrderGens || [];
 
   var withoutArrows = babel.transform(code, {
     retainLines: true,
     plugins: [arrowFunctions]
-  })
+  });
 
   const visitor = {
     CallExpression,
     VariableDeclarator,
     FunctionDeclaration,
     FunctionExpression: FunctionDeclaration
-  }
+  };
 
   const result = babel.transform(withoutArrows.code, {
     plugins: [{ visitor }]
-  })
+  });
 
-  return result.code
+  return result.code;
 
   function CallExpression(path) {
-    var parent = path.parentPath
-    if (parent.node.type !== 'YieldExpression') {
-      if (path.node.callee.type !== 'MemberExpression') {
+    var parent = path.parentPath;
+    if (parent.node.type !== "YieldExpression") {
+      if (path.node.callee.type !== "MemberExpression") {
         const inScope =
           path.scope.bindings[path.node.callee.name] &&
           // path.scope.bindings[path.node.callee.name].type &&
-          path.scope.bindings[path.node.callee.name].type !== 'param'
-        const inFile = path.hub.file.scope.bindings[path.node.callee.name]
-        const deleg = inScope || inFile ? true : false
+          path.scope.bindings[path.node.callee.name].type !== "param";
+        const inFile = path.hub.file.scope.bindings[path.node.callee.name];
+        const deleg = inScope || inFile ? true : false;
         path.replaceWith(
-          babel.types.yieldExpression(
+          t.yieldExpression(
             deleg ? addCallFnWrapper(path, userFnName) : addLineNumber(path),
             deleg
           )
-        )
+        );
       }
     }
   }
@@ -76,65 +76,65 @@ function autoYield(code, generatorNames, secondOrderGens, userFnName) {
   function FunctionDeclaration(path) {
     if (!path.node.generator) {
       path.replaceWith(
-        babel.types[camelCase(path.node.type)](
+        t[camelCase(path.node.type)](
           path.node.id,
           path.node.params,
           path.node.body,
           true
         )
-      )
+      );
     }
   }
 
   function VariableDeclarator(path) {
     if (
-      path.node.init.type === 'FunctionExpression' &&
+      path.node.init.type === "FunctionExpression" &&
       !path.node.init.generator
     ) {
       path.replaceWith(
-        babel.types.variableDeclarator(
+        t.variableDeclarator(
           path.node.id,
-          babel.types[camelCase(path.node.init.type)](
+          t[camelCase(path.node.init.type)](
             path.node.init.id,
             path.node.init.params,
             path.node.init.body,
             true
           )
         )
-      )
+      );
     }
   }
 
   function isGenerator(callee, scope) {
     if (callee.object) {
-      return isMethodGenerator(callee.object.name, callee.property.name, scope)
+      return isMethodGenerator(callee.object.name, callee.property.name, scope);
     } else {
-      return isFunctionGenerator(callee.name, scope)
+      return isFunctionGenerator(callee.name, scope);
     }
   }
 
   function isFunctionGenerator(name, scope) {
-    scope = findBindingScope(name, scope)
+    scope = findBindingScope(name, scope);
     if (scope) {
       return (
         scope.bindings[name].path.node.generator ||
         (!scope.parent && generatorNames.indexOf(name) >= 0)
-      )
+      );
     } else {
-      return generatorNames.indexOf(name) >= 0
+      return generatorNames.indexOf(name) >= 0;
     }
   }
 
   function isMethodGenerator(objectName, propertyName, scope) {
-    scope = findBindingScope(objectName, scope)
+    scope = findBindingScope(objectName, scope);
     //XXX add real support for methods
-    return scope && !scope.parent && generatorNames.indexOf(objectName) >= 0
+    return scope && !scope.parent && generatorNames.indexOf(objectName) >= 0;
   }
 
   function findBindingScope(name, scope) {
     while (scope && !scope.bindings[name]) {
-      scope = scope.parent
+      scope = scope.parent;
     }
-    return scope
+    return scope;
   }
 }
